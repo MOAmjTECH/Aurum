6262da3278602c04992beddb204c698f780381ec
MOAmjTECH <amijimuhammad11@gmail.com>
2026-05-14 23:17:39 +0200

    added base setup

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "amiji123",
+    database: "aurum_finance"
+});
+
+db.connect((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Database connected");
+    }
+});
